c28b597c5d640d48c08473cc744fdee0642e2370
Konark-2ez <konarkkumar456@gmail.com>
2023-06-02 20:34:44 +0530

    added login/logout api

--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const {UserModel}  =require('../model/user.model');
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Redis = require("ioredis");
+const {client} = require("../config/redis")
 const userRouter = express.Router();
 
 userRouter.post("/register",async(req,res)=>{
@@ -19,4 +21,41 @@ userRouter.post("/register",async(req,res)=>{
         res.status(400).send({msg:error.message})
     }
 })
+userRouter.post("/login",async(req,res)=>{
+    const {email,password} = req.body
+    try {
+        const userExist = await UserModel.findOne({email})
+        if(!userExist){
+            res.status(400).send({msg:"User doesnot exist, please register"})
+        }
+        const isPassword = await bcrypt.compare(password,userExist.password)
+        if(!isPassword){
+            res.status(400).send({msg:"Wrong password"})
+        }
+        console.log(userExist.name)
+        const token = jwt.sign({userid:userExist._id,role:userExist.name},process.env.JWT_ACCESS_TOKEN,{expiresIn:"1h"})
+        const refreshToken = jwt.sign({userid:userExist._id,role:userExist.name},process.env.JWT_REFRESh_TOKEN,{expiresIn:"7d"})
+
+        client.mset("token",token,'EX',60)
+        client.mset("refreshToken",refreshToken,'EX',180)
+        res.status(200).send({msg:"Login Successful",token,refreshToken})
+    } catch (error) {
+        res.status(400).send({msg:error.message})
+    }
+})
+
+userRouter.get("/logout",(req,res)=>{
+   client.get("token").then((result) => {
+        console.log(result); // Prints "value"
+        const decoded = jwt.verify(result,process.env.JWT_ACCESS_TOKEN)
+   
+    
+        if(!decoded){
+              res.status(400).send({"msg":"Access not granted"})
+        }
+        client.mset("blackList",decoded.userid)
+        res.status(200).send("Logged Out")
+      });
+})
+
 module.exports = {userRouter}
